4a235256ddabf3d3dbbab88af580ee999e1e0cb9
Rui Ferreira <Rui.Ferreira@corp.eddom.org>
2026-07-13 12:22:12 +0100

    Added Ad creator script

---
+++ ActiveDirectory/Group_creator/ADGroup_Creator.ps1
@@ -0,0 +1,8 @@
+﻿#Importing CSV file for data
+$value = Import-Csv -Path .\Groups.csv
+
+foreach($data in $value)
+{
+     New-ADGroup -Name $data.GroupName -SamAccountName $data.GroupName -GroupCategory Security -GroupScope $data.Type -DisplayName $data.GroupName -Path $data.Location -Description $data.Description
+     Write-Output "Create group " $data.GroupName
+ }
